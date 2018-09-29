5806d790541e0862dab0b9285b0de552a559d1c6
Camila Novaes <novaes.csw@gmail.com>
2018-09-29 15:06:33 -0300

    Arrumando MainActivity

--- androidCar/app/src/main/java/project/phi/androidcar/MainActivity.java
+++ androidCar/app/src/main/java/project/phi/androidcar/MainActivity.java
@@ -1,8 +1,8 @@
 package project.phi.androidcar;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,19 +10,22 @@ import project.phi.androidcar.joystick.joystickActivity;
 import project.phi.androidcar.streaming.streamingActivity;
 
 /*
-    MainActivity irá chamar o joystick ou a camera.
+    TODO: Modificar o layout dessa Activity
+
 */
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bnt_cam = (Button) findViewById(R.id.bnt_camera);
-    Button bnt_joy = (Button) findViewById(R.id.bnt_joystick);
-
+    public Button bnt_cam;
+    public Button bnt_joy;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnt_cam = (Button) findViewById(R.id.bnt_camera);
+        bnt_joy = (Button) findViewById(R.id.bnt_joystick);
 
         bnt_cam.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,10 +43,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-
-
-
 }
+
