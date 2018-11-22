c6e6952afa35b5f9dbeb75db8b35a3d1b34bca60
Pablo Wesley <wesley_pablo68@yahoo.com>
2018-11-21 23:39:23 -0300

    update sockets communication

--- ImageProcessing/recv_buffered_img.py
+++ ImageProcessing/recv_buffered_img.py
@@ -2,68 +2,65 @@ import socket
 import sys
 import cv2 as cv
 import struct
-addr = ('192.168.43.1',9191)
+
+ip = '192.168.43.1'
+addr = (ip,9191)
+addr2 =(ip,9192)
+
+placa_cascade = cv.CascadeClassifier('cascades/placa_pare.xml')
+semaforo_cascade = cv.CascadeClassifier('cascades/semaforo.xml')  # cascade detecta mais pela intensidade
+
+blueColor=(255,0,0)
+redColor=(0,0,255)
+greenColor=(0,255,0)
+
+# Detect vai ser substituida pode NN
+def detect(img,myCascade,color,commSock):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    object = myCascade.detectMultiScale(gray, 1.3, 3)
+    if  len(object) == 0 : print("Empty")
+    else :
+        commSock.send('D'.encode())
+        print('Received from server: ' + str(commSock.recv(1024)))
+    for (x, y, w, h) in object: cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
+
 
 try:
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     print('Socket Initialized')
 except:
     print('Failed to Init Socket')
     sys.exit(0)
 
 try:
-    s.connect(addr)
+    sock1.connect(addr)
+    sock2.connect(addr2)
     print('Socket Connected')
 except:
     print('Connection Failed')
     sys.exit(0)
 
-i=0
-buff= 65536
-
-'''
-Inicia um arquivo que sera finalizado no final de cada iteracao
-Recebe primeiramente uma resposta do servidor caso nao seja dados
-faz uma requisicao para terminar de receber os dados caso não tenha o final 'FEND'
-entao pega o tamanho da imagem e subtrai pelo tamanho do buffer para ver o quanto
-falta para completar a imagem e colocar em modulo e faz o requisicao do tamanho do buffer
-restante para evitar escrita de outra imagem em cima do buffer 
-no final salva a imagem e le com o opencv
-data - tem os dados completos com tamanho da imagem, inicio e fim
-data[10:] - tem somente a imagem
-data[1:5] - indica somente o tamanhho da image
-data[-4:] - tem a flag de fim
-'''
+bufsize = 65536 # Buffer de 64KB
 
 while True:
     with open('shot.jpg','wb') as f:
-        i+=1
-        data = s.recv(buff)
-        if data [-4:] == b'FEND':
-            f.write(data[10:])
+        data = sock1.recv(1)
+        data = sock1.recv(bufsize)
+        imgSize = struct.unpack('>i', data[0:4])
+        if (data[-4:] == b'FEND'):
+            f.write(data[9:])
         else:
-            data+=s.recv(buff)
-            if data[-4:] == b'FEND':
-                f.write(data[10:])
-                print('1',data)
-                print('1',data[10:])
-                print('1',data[1:5])
-                print('1',data[-4:])
-            else:
-                rest=struct.unpack('>i', data[1:5])
-                print(type(rest),rest[0])
-                if (rest[0] < buff): data += s.recv(abs(buff-rest[0]))
-                else: data += s.recv(buff + abs(buff - rest[0]))
-                f.write(data[10:])
-                print('2',data)
-                print('2', data[10:])
-                print('2', data[1:5])
-                print('2', data[-4:])
+            data+= sock1.recv(abs(imgSize[0]-bufsize))
+            f.write(data[9:])
+
         try:
             img = cv.imread('shot.jpg')
+            detect(img, semaforo_cascade, blueColor, sock2)
             cv.imshow("img", img)
             if cv.waitKey(25) == 27: break
         except: print('not image')
     f.close()
-s.close()
 
+sock1.close()
+sock2.close()
