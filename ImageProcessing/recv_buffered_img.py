15e3f119371a6b74158626d26b03631607cb358f
Pablo Wesley <wesley_pablo68@yahoo.com>
2018-11-24 21:33:46 -0300

    update working code

--- ImageProcessing/recv_buffered_img.py
+++ ImageProcessing/recv_buffered_img.py
@@ -14,53 +14,46 @@ blueColor=(255,0,0)
 redColor=(0,0,255)
 greenColor=(0,255,0)
 
-# Detect vai ser substituida pode NN
-def detect(img,myCascade,color,commSock):
+def detect(img,myCascade,color,connSock):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     object = myCascade.detectMultiScale(gray, 1.3, 3)
     if  len(object) == 0 : print("Empty")
-    else :
-        commSock.send('D'.encode())
-        print('Received from server: ' + str(commSock.recv(1024)))
+    else : print("Something Detected")
     for (x, y, w, h) in object: cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
 
-
 try:
-    sock1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     print('Socket Initialized')
 except:
     print('Failed to Init Socket')
     sys.exit(0)
 
 try:
-    sock1.connect(addr)
-    sock2.connect(addr2)
+    s.connect(addr)
     print('Socket Connected')
 except:
     print('Connection Failed')
     sys.exit(0)
 
-bufsize = 65536 # Buffer de 64KB
+bufsize= 65536
 
 while True:
     with open('shot.jpg','wb') as f:
-        data = sock1.recv(1)
-        data = sock1.recv(bufsize)
-        imgSize = struct.unpack('>i', data[0:4])
-        if (data[-4:] == b'FEND'):
-            f.write(data[9:])
+        data = s.recv(bufsize)
+        if data [-4:] == b'FEND':
+            f.write(data[10:])
         else:
-            data+= sock1.recv(abs(imgSize[0]-bufsize))
-            f.write(data[9:])
-
+            data+=s.recv(bufsize)
+            if data[-4:] == b'FEND': f.write(data[10:])
+            else:
+                rest=struct.unpack('>i', data[1:5])
+                data += s.recv(abs(rest[0]-bufsize))
+                f.write(data[10:])
         try:
             img = cv.imread('shot.jpg')
-            detect(img, semaforo_cascade, blueColor, sock2)
+            detect(img, placa_cascade, blueColor, s)
             cv.imshow("img", img)
             if cv.waitKey(25) == 27: break
         except: print('not image')
     f.close()
-
-sock1.close()
-sock2.close()
+s.close()
