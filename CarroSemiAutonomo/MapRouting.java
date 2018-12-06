8b13ef005898e7ba0742b4cae6ca5d80fe14ad7b
Gustavo Moraes Vasconcelos <gmv_facul@hotmail.com>
2018-12-06 05:59:07 -0200

    Exemplo de utilizacao

--- CarroSemiAutonomo/MapRouting.java
+++ CarroSemiAutonomo/MapRouting.java
@@ -1,5 +1,3 @@
-package maprouting;
-
 public class MapRouting {
     
     public static void main(String[] args) {
@@ -7,7 +5,7 @@ public class MapRouting {
        cmd.FinalPoint(13);
        char[] Ida = cmd.getCmdIda(), Volta = cmd.getCmdVolta();
         System.out.print("Ida: ");
-        for(int i=0;i<cmd.getCmdIda().length;i++){
+        for(int i=0;i<Ida.length;i++){
             if(Ida[i] != ' ')
                 System.out.print(Ida[i]);
             else{
@@ -16,7 +14,7 @@ public class MapRouting {
             }
         }
         System.out.print("Volta: ");
-        for(int i=0;i<cmd.getCmdVolta().length;i++){
+        for(int i=0;i<Volta.length;i++){
             if(Volta[i] != ' ')
                 System.out.print(Volta[i]);
             else{
